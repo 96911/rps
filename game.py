8d94c3c213744f0c799f0d414b528f65c40b5d5a
96911 <114050565+96911@users.noreply.github.com>
2022-09-21 09:06:39 +0300

    commit new file

---
+++ game.py
@@ -0,0 +1,13 @@
+human_turn = 'paper'
+computer_turn = 'scissors'
+
+if human_turn == computer_turn
+    print ('draw')
+elif human_turn == 'rock' and computer_turn == 'scissors':
+    print('tu vineju!')
+elif human_turn == 'paper' and computer_turn == 'rock':
+    print('tu vineji!')
+elif human_turn == 'scissors' and computer_turn == 'paper':
+    print('tu vineji!')
+else 
+    print('smh my head tu zaudeji!')
